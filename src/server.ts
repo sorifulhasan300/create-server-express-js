b8e4ad012e33038f2ffdc2aeef284ed34abaab95
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-12-02 00:06:16 +0600

    create data post request

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,19 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = 5000;
 
+// parse data from post
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello next level developer");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  });
 });
 
 app.listen(port, () => {
