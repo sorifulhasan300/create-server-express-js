94194d473b7bc08b429c6a42d8dda9e66aa0e4ca
Md Soriful hasan <sorifullhasan300@gmail.com>
2025-12-02 00:50:50 +0600

    create db with neon db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
 const app = express();
 const port = 5000;
 
 // parse data from post
 app.use(express.json());
-
+const initDB = async () => {
+  `CREATE TABLE IF NOT EXIST users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(15),
+    address TEXT,
+    create_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )`;
+};
+initDB();
+const pool = new Pool({
+  connectionString: `postgresql://neondb_owner:npg_aIL4W8ZDxgNB@ep-jolly-lake-a4mmd9i4-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`,
+});
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello next level developer");
 });
